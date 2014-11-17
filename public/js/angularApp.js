d6adac8c34a2958813458ab678d2ddd194fab97e
aguilarm <mika.aguilar@gmail.com>
2014-11-17 19:18:22 +0000

    Added validation for Link field of posts
    Previously accepted anything which almost always results in a bad link.

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -120,7 +120,24 @@ function($scope, posts){
     $scope.title = '';
     
     $scope.addPost = function(){
-        if($scope.title === '') {return;}
+        if($scope.title.length === 0) {
+            alert('Title is required!');
+            return;
+        }
+        
+        //regex from https://gist.github.com/jpillora/7885636
+        
+        var isValidUrl = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.‌​\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[‌​6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1‌​,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00‌​a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u‌​00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+ 
+        var url = $scope.link;
+        
+        //link is not required, but if present it must be valid
+        
+        if($scope.link && !isValidUrl.test(url)) {
+            alert('You must include a full valid url! (ex: http://www.example.com)');
+            return;
+        }
+            
         posts.create({
             title: $scope.title,
             link: $scope.link,
